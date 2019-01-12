c0efb49863e3c9c2d67b895157d1e76edabd8b53
Phung Pham <phungpham.tech@gmail.com>
2019-01-12 15:18:09 -0500

    updated caches

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,11 @@
 
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[Service Worker] Precatching App Shell');
         cache.addAll([
@@ -20,17 +23,24 @@ self.addEventListener('install', function(event) {
           'https://fonts.googleapis.com/icon?family=Material+Icons',
           'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
         ]);
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
-        
-
-
       })
   )
 });
+// A comment
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== 'static-v3' && key !== 'dynamic') {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  )
   return self.clients.claim();
 });
 
@@ -41,7 +51,17 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         } else {
-          return fetch(event.request);
+          return fetch(event.request)
+          .then(function(res) {
+            return caches.open(CACHE_DYNAMIC_NAME)
+              .then(function(cache) {
+                cache.put(event.request.url, res.clone())
+                return res;
+              })
+          })
+          .catch(function(err) {
+
+          })
         }
       })
   );
